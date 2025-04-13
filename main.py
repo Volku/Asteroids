bfa3f3a2fa01f8d5b8a8c36b5170bdd1fc897b88
Volku <Natchanon_maneechote@hotmail.com>
2025-04-14 01:03:54 +0700

    add tick

--- main.py
+++ main.py
@@ -9,12 +9,16 @@ def main():
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
+    clock = pygame.time.Clock()
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-            return
+                return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60)/1000
+
 
 if __name__ == "__main__":
     main()
