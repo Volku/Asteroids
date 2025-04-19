67146eec373b50772709c0b54f0f57bec216d8fe
Volku <Natchanon_maneechote@hotmail.com>
2025-04-19 18:58:28 +0700

    add asteroid and play system

--- main.py
+++ main.py
@@ -3,6 +3,10 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
+from asteroid import *
+from asteroidfield import *
+from shot import *
 
 def main():
     print('Starting Asteroids!')
@@ -11,11 +15,32 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     dt = 0
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+    player1 = Player(SCREEN_WIDTH /2, SCREEN_HEIGHT/2)
+    asteroid_field = AsteroidField()
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        updatable.update(dt)
+        for asteroid in asteroids:
+            for bullet in shots:
+                if bullet.check_collisions(asteroid):
+                    bullet.kill()
+                    asteroid.split()
+            if player1.check_collisions(asteroid):
+                print("Game over!")
+                return
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
